b34a79468fb073efe20c0bbef6fdc9d1cbdc65b7
qiqigou <2319002545@qq.com>
2021-01-06 01:19:33 +0800

    增加重载接口

--- QuickFrame.Repositorys/QueryFactory/IQueryFactory.cs
+++ QuickFrame.Repositorys/QueryFactory/IQueryFactory.cs
@@ -1,4 +1,5 @@
-﻿using QuickFrame.Models;
+﻿using QuickFrame.Common;
+using QuickFrame.Models;
 using System;
 using System.Linq.Expressions;
 
@@ -19,6 +20,19 @@ namespace QuickFrame.Repositorys
         /// <param name="orderkeys">排序键</param>
         /// <returns></returns>
         IQueryProvider<TView, TQKey> Create<IOption, TView, TQKey>(Expression<Func<TView, TQKey>> qkeys, Expression<Func<TView, object>>? orderkeys = default)
+            where IOption : IContextOption
+            where TView : ViewEntity, new()
+            where TQKey : notnull;
+        /// <summary>
+        /// 构建视图查询提供者
+        /// </summary>
+        /// <typeparam name="IOption">库标志</typeparam>
+        /// <typeparam name="TView">视图类型</typeparam>
+        /// <typeparam name="TQKey">查询键类型</typeparam>
+        /// <param name="qkeys">查询键</param>
+        /// <param name="orderkeys">排序键</param>
+        /// <returns></returns>
+        IQueryProvider<TView, TQKey> Create<IOption, TView, TQKey>(Expression<Func<TView, TQKey>> qkeys, SortInput[]? orderkeys = default)
             where IOption : IContextOption
             where TView : ViewEntity, new()
             where TQKey : notnull;

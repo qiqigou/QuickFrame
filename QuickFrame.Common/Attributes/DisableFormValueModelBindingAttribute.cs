f73947e1300ac5c850e199154e9a2cbe50309bf0
qiqigou <2319002545@qq.com>
2021-01-13 05:21:40 +0800

    添加禁用模型绑定特性

---
+++ QuickFrame.Common/Attributes/DisableFormValueModelBindingAttribute.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+
+namespace QuickFrame.Common
+{
+    /// <summary>
+    /// 禁用模型绑定
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class DisableFormValueModelBindingAttribute : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            var factories = context.ValueProviderFactories;
+            factories.RemoveType<FormValueProviderFactory>();
+            factories.RemoveType<FormFileValueProviderFactory>();
+            factories.RemoveType<JQueryFormValueProviderFactory>();
+            //factories.RemoveType<RouteValueProviderFactory>();
+            //factories.RemoveType<QueryStringValueProviderFactory>();
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+        }
+    }
+}
